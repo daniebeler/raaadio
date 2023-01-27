9a4d994079f3087761b6dc8d0f3bc7612e0d1d44
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2023-01-27 13:39:14 +0100

    implemented swiper

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,10 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
+import SwiperCore, { Keyboard, Pagination, Navigation, Virtual } from 'swiper';
+
+SwiperCore.use([Keyboard, Pagination, Navigation, Virtual]);
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss', ],
+  encapsulation: ViewEncapsulation.None
 })
 export class HomePage implements OnInit {
 
